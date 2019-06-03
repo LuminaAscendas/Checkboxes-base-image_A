e2a6cc8fc1c727064c8dcf05eb8f40f68e954396
rajarajan <rajarajan.rajendran@luminad.com>
2019-06-03 19:26:26 +0530

    First Commit

---
+++ assets/js/data.js
@@ -0,0 +1,1 @@
+var direction_text='<span class="direction">Directions:</span><span class="dir_text"> Click on each step sequentially in the key to view the cascading responses of increased carbon dioxide.</span>'
