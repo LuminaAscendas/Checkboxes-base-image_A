ade07ca5f90d27fc791688e864fa48bb421689f7
Raja <rajarajan.rajendran@luminad.com>
2019-06-12 18:37:22 +0530

    data and script updated

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,1 +1,10 @@
 var direction_text='<span class="direction">Directions:</span><span class="dir_text"> Click on each step sequentially in the key to view the cascading responses of increased carbon dioxide.</span>'
+
+
+
+var checkBoxText=[
+	"Most people know that an increase in atmospheric CO2 leads to global warming, but this phenomenon is far from a simple cause-and-effect relationship. Increasing CO2 may cause a cascade of interacting responses throughout the Earth system.",
+	"Effects of increased atmospheric CO2 on the ocean.",
+	"Effects of increased atmospheric CO2 on land plants and animals",
+	"Positive feedbacks, such as release of methane from melting tundra accelarates climate change"
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -152,6 +152,23 @@ $(document).ready(function(){
 	});
 	
 	$('.checkbox1').on("click",fnClickCheckBox);
+	
+	
+	
+	
+	$(".checkbox1").mouseenter(function(){
+  		$(".checkbox1").removeAttr('title');
+	});
+	
+	
+	$(".checkbox1").mouseleave(function() {
+		
+		for(i=0; i<numberOfCheck; i++){
+			$(".checkbox1").attr("title", checkBoxText[i]);
+		}
+	});
+	
+	
 })
 
 /*Activity start here*/
@@ -184,14 +201,20 @@ var fnClickCheckBox = function(ev){
 			$(this).removeClass("clicked");
 			$('#check_'+indexId).attr("aria-checked", "false");
 			$('.graph_'+indexId).css('display','none');
+			$('#check_'+indexId).removeAttr('title');
 		}else{
 		
 			$(this).addClass("clicked");
 			$('#check_'+indexId).attr("aria-checked", "true");
 			$('.graph_'+indexId).css('display','block');		
+			$('#check_'+indexId).attr('title', checkBoxText[indexId-1]);		
 		}
 
 }
+
+
+
+
 /*On rezize function*/
 
 window.onresize = function() {
